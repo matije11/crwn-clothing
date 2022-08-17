00e1a61bf13f6f5d2b84eb35c639fded5d4c6421
matije11 <matije.vukovic@gmail.com>
2022-08-17 13:17:22 +0200

    added typescript to redux sagas

---
+++ src/store/categories/categorySaga.ts
@@ -0,0 +1,21 @@
+import { takeLatest, all, call, put } from 'typed-redux-saga/macro'
+import { getCategoriesAndDocuments } from '../../utils/firebase/firebase';
+import { fetchCategoriesSuccess, fetchCategoriesFailed } from './categoryAction';
+import { CATEGORY_ACTION_TYPES } from './categoryTypes';
+
+export function* fetchCategoriesAsync() {
+    try {
+        const categoriesArray = yield* call(getCategoriesAndDocuments);
+        yield* put(fetchCategoriesSuccess(categoriesArray));
+    } catch (error) {
+        yield* put(fetchCategoriesFailed(error as Error))
+    }
+}
+
+export function* onFetchCategories() {
+    yield* takeLatest(CATEGORY_ACTION_TYPES.FETCH_CATEGORIES_START, fetchCategoriesAsync)
+}
+
+export function* categoriesSaga() {
+    yield* all([call(onFetchCategories)])
+}
